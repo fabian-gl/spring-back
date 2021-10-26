849ddb05aa61ab86d987ccdb0409f0e3049f232d
fabian-gl <fabianliuni@gmail.com>
2021-10-25 22:48:59 -0300

    Added functionality to login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,79 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const { getDBModel } = require("../config/db");
 
 exports.login = async (req, res) => {
-  const User = getDBModel("User");
+  try {
+    const User = getDBModel("User");
 
-  const savedUser = await User.create({
-    firstName: "pedro",
-    lastName: "Elesc",
-    email: "rewertw",
-    password_hash: "passwordHash",
-  });
-  res.status(200).json({ ok: true, data: "login", savedUser });
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (!user)
+      res.status(401).json({ errors: ["Incorrect email or password"] });
+    else {
+      const authOk = await bcrypt.compare(
+        req.body.password,
+        user.password_hash
+      );
+
+      if (authOk) {
+        jwt.sign(
+          {
+            userId: user.id,
+          },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: "24h" },
+          (err, token) => {
+            if (err) throw new Error(err);
+            res
+              .status(200)
+              .json({ token, name: user.name, message: "Login successful" });
+          }
+        );
+      } else res.status(401).json({ errors: ["Incorrect email or password"] });
+    }
+  } catch (error) {
+    res.status(500).json({ errors: ["Couldn't log you in"] });
+  }
 };
 
 exports.register = async (req, res) => {
-  res.status(200).json({ ok: true, data: "register" });
+  try {
+    const User = getDBModel("User");
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (user)
+      res.status(400).json({ errors: ["The email is already registered"] });
+    else {
+      const hash = await hashPassword(req.body.password);
+
+      await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password_hash: hash,
+      });
+
+      res
+        .status(200)
+        .json({ ok: true, message: "User created, now log in please" });
+    }
+  } catch (error) {
+    res.status(500).json({ ok: false, errors: "Couldn't sign you in" });
+  }
 };
+
+// Helper function
+async function hashPassword(password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) reject(err);
+      bcrypt.hash(password, salt, function (err, hash) {
+        if (err) reject(err);
+        resolve(hash);
+      });
+    });
+  });
+}

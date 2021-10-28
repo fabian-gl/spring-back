553f4cd6890288b5285341b5ce755af4c9b625d7
fabian-gl <fabianliuni@gmail.com>
2021-10-28 02:28:47 -0300

    Added maxAge for photo cache

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,19 +1,24 @@
 const fetch = require("node-fetch");
 
 let cachedPhotos;
+let timestampLastFetch;
+const ONE_HOUR = 1000 * 60 * 60;
 
 exports.getAll = async (req, res) => {
   const offset = +req.query.offset || 0;
   const limit = +req.query.limit || 10;
-  try {
-    if (!cachedPhotos) cachedPhotos = await fetchPhotos();
+  const now = Date.now();
 
-    res
-      .status(200)
-      .json({
-        count: cachedPhotos.length,
-        photos: cachedPhotos.slice(offset, offset + limit),
-      });
+  try {
+    if (!cachedPhotos || now - timestampLastFetch > ONE_HOUR) {
+      cachedPhotos = await fetchPhotos();
+      timestampLastFetch = now;
+    }
+
+    res.status(200).json({
+      count: cachedPhotos.length,
+      photos: cachedPhotos.slice(offset, offset + limit),
+    });
   } catch (error) {
     res.status(500).json({ message: "Couldn't fetch data" });
   }
